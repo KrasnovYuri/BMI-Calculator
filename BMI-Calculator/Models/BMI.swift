d6b4dffb4a59322dcd552eea35abf839a68bf7b7
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-10 19:23:01 +0300

    final

---
+++ BMI-Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI-Calculator
+//
+//  Created by Юрий on 10.11.2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
